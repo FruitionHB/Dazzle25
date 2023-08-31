b2bd5062c0c34ebf9f08b28b6bd054c2f70eb727
LarryAMorgan <107962483+LarryAMorgan@users.noreply.github.com>
2023-08-31 16:32:56 +1200

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,7 +1,19 @@
 var wms_layers = [];
 
 
-        var lyr_ESRIImagery_0 = new ol.layer.Tile({
+        var lyr_GoogleSatellite_0 = new ol.layer.Tile({
+            'title': 'Google Satellite',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+            })
+        });
+
+        var lyr_ESRIImagery_1 = new ol.layer.Tile({
             'title': 'ESRI Imagery',
             'type': 'base',
             'opacity': 1.000000,
@@ -12,26 +24,26 @@ var wms_layers = [];
                 url: 'https://server.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
             })
         });
-var format_Dazzleblocks_1 = new ol.format.GeoJSON();
-var features_Dazzleblocks_1 = format_Dazzleblocks_1.readFeatures(json_Dazzleblocks_1, 
+var format_Dazzle_2 = new ol.format.GeoJSON();
+var features_Dazzle_2 = format_Dazzle_2.readFeatures(json_Dazzle_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Dazzleblocks_1 = new ol.source.Vector({
+var jsonSource_Dazzle_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Dazzleblocks_1.addFeatures(features_Dazzleblocks_1);
-var lyr_Dazzleblocks_1 = new ol.layer.Vector({
+jsonSource_Dazzle_2.addFeatures(features_Dazzle_2);
+var lyr_Dazzle_2 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_Dazzleblocks_1, 
-                style: style_Dazzleblocks_1,
+                source:jsonSource_Dazzle_2, 
+                style: style_Dazzle_2,
                 interactive: true,
-                title: '<img src="styles/legend/Dazzleblocks_1.png" /> Dazzle blocks'
+                title: '<img src="styles/legend/Dazzle_2.png" /> Dazzle'
             });
 
-lyr_ESRIImagery_0.setVisible(true);lyr_Dazzleblocks_1.setVisible(true);
-var layersList = [lyr_ESRIImagery_0,lyr_Dazzleblocks_1];
-lyr_Dazzleblocks_1.set('fieldAliases', {'ID': 'ID', 'Grower': 'Grower', 'Address': 'Address', 'Region': 'Region', 'RPIN': 'RPIN', 'Block_name': 'Block_name', 'Yr_planted': 'Yr_planted', 'Row_width': 'Row_width', 'Tree_space': 'Tree_space', 'Density': 'Density', 'Area (ha)': 'Area (ha)', 'Calc_trees': 'Calc_trees', 'Licence_trees': 'Licence_trees', 'Photo': 'Photo', });
-lyr_Dazzleblocks_1.set('fieldImages', {'ID': 'Range', 'Grower': 'TextEdit', 'Address': 'TextEdit', 'Region': 'TextEdit', 'RPIN': 'TextEdit', 'Block_name': 'TextEdit', 'Yr_planted': 'TextEdit', 'Row_width': 'TextEdit', 'Tree_space': 'TextEdit', 'Density': 'TextEdit', 'Area (ha)': 'TextEdit', 'Calc_trees': 'TextEdit', 'Licence_trees': 'TextEdit', 'Photo': 'ExternalResource', });
-lyr_Dazzleblocks_1.set('fieldLabels', {'ID': 'inline label', 'Grower': 'inline label', 'Address': 'inline label', 'Region': 'inline label', 'RPIN': 'inline label', 'Block_name': 'inline label', 'Yr_planted': 'inline label', 'Row_width': 'inline label', 'Tree_space': 'inline label', 'Density': 'inline label', 'Area (ha)': 'inline label', 'Calc_trees': 'inline label', 'Licence_trees': 'inline label', 'Photo': 'no label', });
-lyr_Dazzleblocks_1.on('precompose', function(evt) {
+lyr_GoogleSatellite_0.setVisible(true);lyr_ESRIImagery_1.setVisible(true);lyr_Dazzle_2.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_ESRIImagery_1,lyr_Dazzle_2];
+lyr_Dazzle_2.set('fieldAliases', {'ID': 'ID', 'Grower': 'Grower', 'Address': 'Address', 'Region': 'Region', 'RPIN': 'RPIN', 'Block_name': 'Block name', 'Yr_planted': 'Year planted', 'Row_width': 'Row width', 'Tree_space': 'Tree space', 'Density': 'Density', 'Area (ha)': 'Area (ha)', 'Calc_trees': 'Calculated trees', 'Licence_trees': 'Licenced trees', 'Labels_2': 'Label', });
+lyr_Dazzle_2.set('fieldImages', {'ID': 'Range', 'Grower': 'TextEdit', 'Address': 'TextEdit', 'Region': 'TextEdit', 'RPIN': 'TextEdit', 'Block_name': 'TextEdit', 'Yr_planted': 'TextEdit', 'Row_width': 'TextEdit', 'Tree_space': 'TextEdit', 'Density': 'TextEdit', 'Area (ha)': 'TextEdit', 'Calc_trees': 'TextEdit', 'Licence_trees': 'TextEdit', 'Labels_2': 'TextEdit', });
+lyr_Dazzle_2.set('fieldLabels', {'ID': 'inline label', 'Grower': 'inline label', 'Address': 'inline label', 'Region': 'inline label', 'RPIN': 'inline label', 'Block_name': 'inline label', 'Yr_planted': 'inline label', 'Row_width': 'inline label', 'Tree_space': 'inline label', 'Density': 'inline label', 'Area (ha)': 'inline label', 'Calc_trees': 'inline label', 'Licence_trees': 'inline label', 'Labels_2': 'inline label', });
+lyr_Dazzle_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -1,35 +1,4 @@
 
-isTracking = false;
-var geolocateControl = (function (Control) {
-    geolocateControl = function(opt_options) {
-        var options = opt_options || {};
-        var button = document.createElement('button');
-        button.className += ' fa fa-map-marker';
-        var handleGeolocate = function() {
-            if (isTracking) {
-                map.removeLayer(geolocateOverlay);
-                isTracking = false;
-          } else if (geolocation.getTracking()) {
-                map.addLayer(geolocateOverlay);
-                map.getView().setCenter(geolocation.getPosition());
-                isTracking = true;
-          }
-        };
-        button.addEventListener('click', handleGeolocate, false);
-        button.addEventListener('touchstart', handleGeolocate, false);
-        var element = document.createElement('div');
-        element.className = 'geolocate ol-unselectable ol-control';
-        element.appendChild(button);
-        ol.control.Control.call(this, {
-            element: element,
-            target: options.target
-        });
-    };
-    if (Control) geolocateControl.__proto__ = Control;
-    geolocateControl.prototype = Object.create(Control && Control.prototype);
-    geolocateControl.prototype.constructor = geolocateControl;
-    return geolocateControl;
-}(ol.control.Control));
 
 var container = document.getElementById('popup');
 var content = document.getElementById('popup-content');
@@ -51,7 +20,7 @@ var expandedAttribution = new ol.control.Attribution({
 
 var map = new ol.Map({
     controls: ol.control.defaults({attribution:false}).extend([
-        expandedAttribution,new geolocateControl()
+        expandedAttribution
     ]),
     target: document.getElementById('map'),
     renderer: 'canvas',
@@ -69,8 +38,8 @@ layerSwitcher.showPanel();
 
 
     var searchLayer = new SearchLayer({
-      layer: lyr_Dazzleblocks_1,
-      colName: 'Grower',
+      layer: lyr_Dazzle_2,
+      colName: 'Region',
       zoom: 10,
       collapsed: true,
       map: map
@@ -81,7 +50,7 @@ layerSwitcher.showPanel();
     .getElementsByTagName('button')[0].className +=
     ' fa fa-binoculars';
     
-map.getView().fit([19663701.411187, -4824143.336752, 19706207.866232, -4798533.931744], map.getSize());
+map.getView().fit([19660757.147877, -4824839.112932, 19718084.110088, -4791148.498217], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
@@ -121,8 +90,8 @@ var featureOverlay = new ol.layer.Vector({
     updateWhileInteracting: true // optional, for instant visual feedback
 });
 
-var doHighlight = false;
-var doHover = true;
+var doHighlight = true;
+var doHover = false;
 
 var highlight;
 var autolinker = new Autolinker({truncate: {length: 30, location: 'smart'}});
@@ -395,44 +364,6 @@ map.on('singleclick', function(evt) {
 });
 
 
-
-      var geolocation = new ol.Geolocation({
-  projection: map.getView().getProjection()
-});
-
-
-var accuracyFeature = new ol.Feature();
-geolocation.on('change:accuracyGeometry', function() {
-  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
-});
-
-var positionFeature = new ol.Feature();
-positionFeature.setStyle(new ol.style.Style({
-  image: new ol.style.Circle({
-    radius: 6,
-    fill: new ol.style.Fill({
-      color: '#3399CC'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#fff',
-      width: 2
-    })
-  })
-}));
-
-geolocation.on('change:position', function() {
-  var coordinates = geolocation.getPosition();
-  positionFeature.setGeometry(coordinates ?
-      new ol.geom.Point(coordinates) : null);
-});
-
-var geolocateOverlay = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    features: [accuracyFeature, positionFeature]
-  })
-});
-
-geolocation.setTracking(true);
 
 
 var attributionComplete = false;
